09cd4f8c909cff1abd932268e2abc5d7a7f0d79a
naufal dsw <naufaldsw87@gmail.com>
2020-02-04 13:14:21 +0700

    view ke-2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,105 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <title>Deepor I Login</title>
+    <meta name="description" content="A responsive bootstrap 4 admin dashboard template by hencework" />
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('favicon.ico')}}">
+    <link rel="icon" href="{{ asset('favicon.ico')}}" type="image/x-icon">
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Toggles CSS -->
+    <link href="{{ asset('deepor/vendor/jquery-toggles/css/toggles.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('deepor/vendor/jquery-toggles/css/themes/toggles-light.css')}}" rel="stylesheet" type="text/css">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    <!-- Custom CSS -->
+    <link href="{{ asset('deepor/dist/css/style.css')}}" rel="stylesheet" type="text/css">
+</head>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
+<body>
+
+    <!-- HK Wrapper -->
+    <div class="hk-wrapper">
+
+        <!-- Main Content -->
+        <div class="hk-pg-wrapper hk-auth-wrapper">
+            <header class="d-flex justify-content-end align-items-center">
+                <div class="btn-group btn-group-sm">
+                    <a href="#" class="btn btn-outline-secondary">Help</a>
+                    <a href="#" class="btn btn-outline-secondary">About Us</a>
+                </div>
+            </header>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xl-12 pa-0">
+                        <div class="auth-form-wrap pt-xl-0 pt-70">
+                            <div class="auth-form w-xl-30 w-lg-55 w-sm-75 w-100">
+                                <a class="d-flex auth-brand align-items-center justify-content-center  mb-20" href="#">
+                                    <img class="brand-img d-inline-block mr-5" src="{{ asset('deepor/dist/img/logo.png')}}" alt="brand" /><span class="text-dark font-23">Deepor</span>
+                                </a>
+                                <form method="POST" action="{{ route('login')}}" class="needs-validation" novalidate="">
+                                    @csrf
+                                    <h1 class="display-4 text-center mb-10">Welcome Back :)</h1>
+                                    <p class="text-center mb-30">Sign in to your account and enjoy unlimited perks.</p>
+                                    <div class="form-group">
+                                        <input class="form-control" type="email" id="email" name="email" required autofocus>
+                                        <div class="invalid-feedback">
+                                            Please fill in your email
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <input class="form-control" placeholder="Password" type="password" id="password" name="password" required>
+                                            <div class="invalid-feedback">
+                                            Please fill in your email
+                                        </div>
+                                            <div class="input-group-append">
+                                                <span class="input-group-text"><span class="feather-icon"><i data-feather="eye-off"></i></span></span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="custom-control custom-checkbox mb-25">
+                                        <input class="custom-control-input" id="same-address" type="checkbox" checked>
+                                        <label class="custom-control-label font-14" for="same-address">Keep me logged in</label>
+                                    </div>
+                                    <div class="form-group">
+                                    <button class="btn btn-warning btn-block" type="submit">Login</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
+        <!-- /Main Content -->
+
     </div>
-</div>
-@endsection
+    <!-- /HK Wrapper -->
+
+    <!-- JavaScript -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('deepor/vendor/jquery/deepor/dist/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{ asset('deepor/vendor/popper.js/deepor/dist/umd/popper.min.js')}}"></script>
+    <script src="{{ asset('deepor/vendor/bootstrap/deepor/dist/js/bootstrap.min.js')}}"></script>
+
+    <!-- Slimscroll JavaScript -->
+    <script src="{{ asset('deepor/dist/js/jquery.slimscroll.js')}}"></script>
+
+    <!-- Fancy Dropdown JS -->
+    <script src="{{ asset('deepor/dist/js/dropdown-bootstrap-extended.js')}}"></script>
+
+    <!-- FeatherIcons JavaScript -->
+    <script src="{{ asset('deepor/dist/js/feather.min.js')}}"></script>
+
+    <!-- Init JavaScript -->
+    <script src="{{ asset('deepor/dist/js/init.js')}}"></script>
+</body>
+
+</html>
---
+++ resources/views/layouts/components/script.blade.php
@@ -0,0 +1,45 @@
+<!-- jQuery -->
+<script src="{{ asset('deepor/vendors/jquery/deepor/dist/jquery.min.js')}}"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="{{ asset('deepor/vendors/popper.js/deepor/dist/umd/popper.min.js')}}"></script>
+<script src="{{ asset('deepor/vendors/bootstrap/deepor/dist/js/bootstrap.min.js')}}"></script>
+
+<!-- Slimscroll JavaScript -->
+<script src="{{ asset('deepor/dist/js/jquery.slimscroll.js')}}"></script>
+
+<!-- Fancy Dropdown JS -->
+<script src="{{ asset('deepor/dist/js/dropdown-bootstrap-extended.js')}}"></script>
+
+<!-- FeatherIcons JavaScript -->
+<script src="{{ asset('deepor/dist/js/feather.min.js')}}"></script>
+
+<!-- Toggles JavaScript -->
+<script src="{{ asset('deepor/vendors/jquery-toggles/toggles.min.js')}}"></script>
+<script src="{{ asset('deepor/dist/js/toggle-data.js')}}"></script>
+
+<!-- Counter Animation JavaScript -->
+<script src="{{ asset('deepor/vendors/waypoints/lib/jquery.waypoints.min.js')}}"></script>
+<script src="{{ asset('deepor/vendors/jquery.counterup/jquery.counterup.min.js')}}"></script>
+
+<!-- Sparkline JavaScript -->
+<script src="{{ asset('deepor/vendors/jquery.sparkline/deepor/dist/jquery.sparkline.min.js')}}"></script>
+
+<!-- Vector Maps JavaScript -->
+<script src="{{ asset('deepor/vendors/vectormap/jquery-jvectormap-2.0.3.min.js')}}"></script>
+<script src="{{ asset('deepor/vendors/vectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+<script src="{{ asset('deepor/dist/js/vectormap-data.js')}}"></script>
+
+<!-- Owl JavaScript -->
+<script src="{{ asset('deepor/vendors/owl.carousel/deepor/dist/owl.carousel.min.js')}}"></script>
+
+<!-- Toastr JS -->
+<script src="{{ asset('deepor/vendors/jquery-toast-plugin/deepor/dist/jquery.toast.min.js')}}"></script>
+
+<!-- Apex JavaScript -->
+<script src="{{ asset('deepor/vendors/apexcharts/deepor/dist/apexcharts.min.js')}}"></script>
+<script src="{{ asset('deepor/dist/js/irregular-data-series.js')}}"></script>
+
+<!-- Init JavaScript -->
+<script src="{{ asset('deepor/dist/js/init.js')}}"></script>
+<script src="{{ asset('deepor/dist/js/dashboard-data.js')}}"></script>
